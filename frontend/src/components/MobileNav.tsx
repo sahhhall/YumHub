a3f852ded27eb4240963b2a5ad549178cbb35037
sahhhall <sahalvavoor313@gmail.com>
2024-03-10 17:07:38 +0530

    feat: Add sign-in and authentication dropdown for mobile display
    - Implemented sign-in and authentication dropdown functionality for improved mobile display

--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -1,23 +1,53 @@
-import { Menu } from "lucide-react"
-import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "./ui/sheet"
-import { Button } from "./ui/button"
+import { Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Button } from "./ui/button";
+import { useState } from "react";
+import Modal from "./Authentication/authModal/AuthModal.tsx";
+import { AuthDropdown } from "./AuthDropdown/AuthDropdown";
+import { useSelector } from "react-redux";
 
 export const MobileNav = () => {
-    return(
-        <Sheet>
-            <SheetTrigger>
-                <Menu />
-            </SheetTrigger>
-            <SheetContent>
-                <SheetTitle>
-                    <span></span>
-                </SheetTitle>
-                <SheetDescription className="flex py-4">
-                    <Button className="flex-1 text-white  font-bold " >
-                        Sign In
-                    </Button>
-                </SheetDescription>
-            </SheetContent>
-        </Sheet>
-    )
-}
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const { isAuthenticated } = useSelector((state: any) => state.user);
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Menu />
+      </SheetTrigger>
+      <SheetContent>
+        <SheetTitle>
+          <span></span>
+        </SheetTitle>
+        <SheetDescription className="flex py-4">
+          {!isAuthenticated ? (
+            <>
+              <Button
+                className="flex-1 text-white  font-bold "
+                onClick={() => setShowModal(true)}
+              >
+                Sign In
+              </Button>
+              <Modal
+                isVisible={showModal}
+                handleClose={() => setShowModal(false)}
+              />
+            </>
+          ) : (
+            <AuthDropdown  />
+          )}
+
+          <Modal
+            isVisible={showModal}
+            handleClose={() => setShowModal(false)}
+          />
+        </SheetDescription>
+      </SheetContent>
+    </Sheet>
+  );
+};
