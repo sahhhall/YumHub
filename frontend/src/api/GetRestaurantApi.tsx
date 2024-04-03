fe0f7e5b4ed6619e5903ea7c6b8cc6ab0126680f
sahhhall <sahalvavoor313@gmail.com>
2024-04-03 14:04:04 +0530

    feat: Implement restaurant location retrieval API using useQuery
    This commit adds functionality to fetch restaurant location information using the useQuery hook from React Query.
    
    Pending tasks:
    - Design restaurant card component.

--- frontend/src/api/GetRestaurantApi.tsx
+++ frontend/src/api/GetRestaurantApi.tsx
@@ -1,29 +1,58 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import axios from "axios";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
+const APIkey = import.meta.env.VITE_API_OPENCAGEDATA;
 
 export const useGetNearbyRestaurants = () => {
-    
-    const sentGeoLocation = async ( res: any ) => { 
-      const response =  await axios.post(
-            `${API_BASE_URL}/api/nearest-store`,
-             res
-        )
-        return response.request.response;
-    }   
-    const {
-        mutate: getNearestRestaraunts,
-        isPending: isPending ,
-        data
-    } = useMutation({
-        mutationFn: sentGeoLocation,
+  const sentGeoLocation = async (res: any) => {
+    const response = await axios.post(`${API_BASE_URL}/api/nearest-store`, res);
+    return response.request.response;
+  };
+  const {
+    mutate: getNearestRestaraunts,
+    isPending: isPending,
+    data,
+  } = useMutation({
+    mutationFn: sentGeoLocation,
+  });
 
-    })
-   
-    return {
-        isPending,
-        getNearestRestaraunts,
-        restaurants: data
-      };
-}
+  return {
+    isPending,
+    getNearestRestaraunts,
+    restaurants: data,
+  };
+};
+
+
+export const useGetRestaurantLocation = (
+  latitude: number,
+  longitude: number
+) => {
+  const getLocationInfo = async () => {
+    try {
+      console.log(latitude, longitude);
+
+      const response = await fetch(
+        `https://api.opencagedata.com/geocode/v1/json?q=${latitude},${longitude}&key=${APIkey}`
+      );
+      const data = await response.json();
+
+      console.log(data);
+      if (data.status.code === 200) {
+        console.log("results:", data.results);
+        return data.results[0].formatted;
+      } else {
+        throw new Error("Error fetching location information");
+      }
+    } catch (error) {
+      console.error(error);
+      throw new Error("Error fetching location information");
+    }
+  };
+
+  const { isLoading, data } = useQuery({
+    queryKey: [latitude, longitude],
+    queryFn: getLocationInfo,
+  });
+  return { isLoading, location: data };
+};
--- frontend/src/components/RestaurantCard.tsx
+++ frontend/src/components/RestaurantCard.tsx
@@ -1,17 +1,26 @@
+import { useGetRestaurantLocation } from "@/api/GetRestaurantApi";
 
+export const RestaurantCard = ({ restaurant }: any) => {
+  const latitude = restaurant.location.coordinates[1];
+  const longitude = restaurant.location.coordinates[0];
+  const { location } = useGetRestaurantLocation(latitude, longitude);
 
-export const RestaurantCard = ( { restaurant } : any ) => {
-    const km = (restaurant.distance / 1000).toFixed(1) as unknown;
-    const estimatedDelivery = km as any* restaurant.openingHours;
-    return (
-        <div>
-            <img src={restaurant.imageUrl} alt="" />
-            <h1 >{restaurant.restaurantName}</h1>
-            <div>
-                <p>{estimatedDelivery}</p>
-            </div>
-            <p>{(restaurant.distance / 1000).toFixed(1)} km</p>
-            <p>{restaurant.location.coordinates}</p>
-        </div>
-    )
-}
+  const calculateEstimatedDelivery = () => {
+    const km = (restaurant.distance / 1000).toFixed(1);
+    return (Number(km) * restaurant.openingHours).toFixed(1);
+  };
+
+  return (
+    <div>
+      <img src={restaurant.imageUrl} alt="" />
+      <h1>{restaurant.restaurantName}</h1>
+      <div>
+        <p>Estimated Delivery Time: {calculateEstimatedDelivery()} hours</p>
+      </div>
+      <p>Distance: {(restaurant.distance / 1000).toFixed(1)} km</p>
+      <p>Location: {location}</p>
+    </div>
+  );
+};
+
+export default RestaurantCard;
